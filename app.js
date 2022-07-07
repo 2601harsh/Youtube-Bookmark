a67de6493db8552c27a4381b663d2cb01ce9d798
2601harsh <87669561+2601harsh@users.noreply.github.com>
2022-07-07 23:01:24 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const os = require('os');
-const currenOS={
-    name : os.type(),
-    release : os.release(),
-    totalmem: os.totalmem(),
-    freemem: os.freemem()
-
-};
-console.log(currenOS);
